bf35f28946f4954a131f14233be344c6d33da34f
Mark Lotspaih <lotcom@users.noreply.github.com>
2016-06-07 21:34:26 -0400

    Create Chap6PracPrintTable.py

---
+++ Chap6PracPrintTable.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+# Table Printer Chap. 6
+# Function for taking lists of strings and displays in an organized table
+
+tableData = [['apples','oranges','cherries','bananas'],
+             ['Alice','Bob','Carol','David'],
+             ['dogs','cats','moose','goose']]
+
+def printTable(dataLists):
+    #Complete function here
+
+printTable(tableData)

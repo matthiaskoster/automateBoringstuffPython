7726925e5cd63e1d311a4e28a44f07801ddb6c6d
Mark Lotspaih <lotcom@users.noreply.github.com>
2016-06-08 22:20:20 -0400

    Update Chap6PracPrintTable.py
    Updated printTable function. Works as requested, but not the best option for iterating sublists in lists?

--- Chap6PracPrintTable.py
+++ Chap6PracPrintTable.py
@@ -8,6 +8,15 @@ tableData = [['apples','oranges','cherries','bananas'],
              ['dogs','cats','moose','goose']]
 
 def printTable(dataLists):
-    #Complete function here
+    colWidths = [0] * len(dataLists)
+    for i in colWidths:
+        colWidths = max(dataLists[i], key=len)
+           
+    y = len(colWidths)
+        
+    for x in range(len(dataLists[0])):
+        print(str(dataLists[0][x]).rjust(y) + str(dataLists[1][x]).rjust(y) + str(dataLists[2][x]).rjust(y))
+
 
 printTable(tableData)
+

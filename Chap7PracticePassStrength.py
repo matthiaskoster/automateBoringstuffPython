ca69b615db8cb8325ac68921c935633db21963cb
Mark Lotspaih <lotcom@users.noreply.github.com>
2016-06-19 10:44:04 -0400

    Added final regex examples for checks. Completed

--- Chap7PracticePassStrength.py
+++ Chap7PracticePassStrength.py
@@ -11,10 +11,16 @@ def passStrengthTest(passWord):
     upperRegex = re.compile(r'[A-Z]')
     numRegex = re.compile(r'[0-9]')
     molower = lowerRegex.search(passWord)
+    moupper = upperRegex.search(passWord)
+    monum = numRegex.search(passWord)
     if len(passWord) < 8:
         print('Your password is less than 8 characters which is too short.')
     elif not molower:
         print('You need at least one lower case letter.')
+    elif not moupper:
+        print('You need at least one upper case letter.')
+    elif not monum:
+        print('You need at least one number.')
     else:
         print('Your password is strong!')
 
@@ -24,3 +30,4 @@ passW = input()
 passStrengthTest(passW)
 
 sys.exit()
+
